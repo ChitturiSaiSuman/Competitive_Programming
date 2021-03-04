bf8a9da39bf826e319b88463746833502730a1ff
ChitturiSaiSuman <saisumanchitturi@gmail.com>
2021-03-04 10:11:16 +0530

    Updated Files

---
+++ Hackerrank/Poisonous_Plants/p1/sol.py
@@ -0,0 +1,160 @@
+"""
+Author: Chitturi Sai Suman
+Created: 2021-03-04 09:48:43
+"""
+# 
+# Linkedin: https://www.linkedin.com/in/sai-suman-chitturi-9727a2196/
+# Hackerrank: https://www.hackerrank.com/skynetasspyder?hr_r=1
+# Codechef: https://www.codechef.com/users/suman_18733097
+# Codeforces: http://codeforces.com/profile/saisumanchitturi
+# Github: https://github.com/ChitturiSaiSuman
+# Hackerearth: https://www.hackerearth.com/@chitturi7
+# SPOJ: Sai Suman Chitturi @out_of_bound
+# 
+
+#  _____   _    _   __    __     ____     __    _
+# / ____| | |  | | |  \  /  |   /    \   |  \  | |
+# | |___  | |  | | |   \/   |  /   _  \  | . \ | |
+# \____ \ | |  | | | |\__/| | |   /_\  | | |\ \| |
+# ____| | | \__/ | | |    | | |   __   | | | \ ` |
+# |_____/ \______/ |_|    |_| |__|  |__| |_|  \__|
+# 
+
+from sys import stdin,stdout,stderr,setrecursionlimit
+from math import pi,sqrt,gcd,ceil,floor,log2,log10,factorial
+from math import cos,acos,tan,atan,atan2,sin,asin,radians,degrees,hypot
+from bisect import insort,insort_left,insort_right,bisect_left,bisect_right,bisect
+from functools import reduce
+from itertools import combinations,combinations_with_replacement,permutations
+from fractions import Fraction
+from random import choice,getrandbits,randint,random,randrange,shuffle
+from re import compile,findall,escape,search,match
+from statistics import mean,median,mode
+from heapq import heapify,heappop,heappush,heappushpop,heapreplace,merge,nlargest,nsmallest
+from collections import deque,OrderedDict,defaultdict
+from collections import Counter,namedtuple,ChainMap,UserDict,UserList,UserString
+# from numpy import dot,trace,argmax,argmin,array,cumprod,cumsum,matmul
+
+shit = 998244353
+mod = 10**9+7
+hell = 10**9+9
+inf = 10**18
+lcm = lambda x,y: ((x*y)//gcd(x,y))
+add = lambda x,y: (x%mod+y%mod)%mod
+sub = lambda x,y: ((x%mod-y%mod)+mod)%mod
+mul = lambda x,y: ((x%mod)*(y%mod))%mod
+inverse = lambda x: (pow(x,mod-2,mod))
+setBitCount = lambda x: bin(x).count("1")
+sumOfDigits = lambda x: sum([int(i) for i in str(x)])
+
+size = 10**6+1
+
+setrecursionlimit(size)
+
+def abort():
+    raise AssertionError("Abort Called")
+
+def preCompute():
+
+    return
+
+def getStacks(l, n):
+    stacks = []
+    i = 0
+    while i < n:
+        temp = []
+        while i < n - 1 and l[i] >= l[i + 1]:
+            temp.append(l[i])
+            i += 1
+        temp.append(l[i])
+        stacks.append(temp)
+        temp = []
+        i += 1
+    return stacks
+
+def solve(l, n):
+    io = IO()
+    stacks = getStacks(l, n)
+    ans = 0
+    n = len(stacks)
+    while len(stacks) > 1:
+        ans += 1
+        i = 1
+        while i < n:
+            # io.debug(stacks, i, n, ans)
+            stacks[i].pop(0)
+            if stacks[i] == []:
+                stacks.pop(i)
+                n -= 1
+                continue
+            if stacks[i-1][-1] >= stacks[i][0]:
+                stacks[i-1].extend(stacks[i])
+                stacks.pop(i)
+                n -= 1
+            else:
+                i += 1
+    # io.debug(stacks, i, n)
+    io.write(ans)
+
+def main():
+    io = IO()
+    testcases = 1
+    if testcases == 0:
+        testcases = io.nextInt()
+    preCompute()
+
+    for test in range(testcases):
+        # io.write("Case #%d: "%(test+1),end="")
+        n = io.Int()
+        l = io.List()
+        solve(l, n)
+
+
+
+class IO:
+    def next(self):
+        return stdin.readline().strip()
+    def nextLine(self):
+        return self.next()
+    def String(self):
+        return self.next()
+    def nextStrings(self):
+        return list(map(str,self.next().split()))
+    def nextInt(self):
+        return int(self.next())
+    def Int(self):
+        return self.nextInt()
+    def nextFloat(self):
+        return float(self.next())
+    def Float(self):
+        return self.nextFloat()
+    def nextList(self):
+        return list(map(int,self.next().split()))
+    def List(self):
+        return self.nextList()
+    def nextTuple(self):
+        return tuple(map(int,self.next().split()))
+    def Tuple(self):
+        return self.nextTuple()
+    def debug(self,*obj,sep=" ",end="\n"):
+        string = sep.join([str(item) for item in obj])+end
+        stderr.write(string)
+    def print(self,*obj,sep=" ",end='\n'):
+        string = sep.join([str(item) for item in obj])+end
+        stdout.write(string)
+    def write(self,*obj,sep=" ",end="\n"):
+        self.print(*obj,sep=sep,end=end)
+    def yes(self):
+        self.write("yes")
+    def Yes(self):
+        self.write("Yes")
+    def YES(self):
+        self.write("YES")
+    def no(self):
+        self.write("no")
+    def No(self):
+        self.write("No")
+    def NO(self):
+        self.write("NO")
+
+main()
---
+++ Hackerrank/Poisonous_Plants/p1/test.py
@@ -0,0 +1,4 @@
+"""
+Author: Chitturi Sai Suman
+Created: 2021-03-04 09:48:43
+"""
